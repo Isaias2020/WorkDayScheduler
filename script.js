3bdf3d9d2d6a26ffd3afc141b540211d7c58246e
Isaias Quintanilla <quintanillaisaias@gmail.com>
2020-04-27 15:16:07 -0500

    tested 1st time block to see if inserted text saved into local storage

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ $(document).ready(function () {
     var todaysDate = moment().format('dddd, MMMM Do YYYY, h:mm:ss a');
     ptagDate.text(todaysDate);
 });
+
+$("#clear").click(function () {
+    localStorage.clear();
+    location.reload()
+});
+
+var insertedText1 = document.querySelector("#note1");
+var saveButton1 = document.querySelector("#saveNote1");
+insertedText1.value = localStorage.getItem("input1");
+saveNote1.addEventListener("click", updatedText1);
+function updatedText1() {
+    localStorage.setItem("input1", insertedText1.value);
+};
