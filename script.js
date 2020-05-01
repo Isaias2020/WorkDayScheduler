cb250f88ffe2674a6d87ac3060f19e7b23ac1b38
Isaias Quintanilla <quintanillaisaias@gmail.com>
2020-05-01 12:31:44 -0500

    made final changes

--- script.js
+++ script.js
@@ -7,48 +7,61 @@ $(document).ready(function () {
 
 //-------------------array for each time block-------------------//
 var timesOfDay = ["09AM", "10AM", "11AM"
-    , "12PM", "1PM", "2PM", "3PM", "4PM", "5PM",]
+    , "12PM", "1PM", "2PM", "3PM", "4PM", "5PM",];
 
 for (var i = 1; i < timesOfDay.length + 1; i++) {
-    var container = $("<div>")
-    container.attr("class", "container")
+    var container = $("<div>");
+    container.attr("class", "container");
 
-    var row = $("<div>")
-    row.attr("class", "row")
+    var row = $("<div>");
+    row.attr("class", "row");
 
-    var timeBlock = $("<div>")
-    timeBlock.attr("class", "time-block past")
-    timeBlock.text(timesOfDay[i - 1])
+    var timeBlock = $("<div>");
+    timeBlock.attr("class", "time-block past bg-secondary");
+    timeBlock.text(timesOfDay[i - 1]);
 
     //--form for inserted text && color code for "past", "present", "future"--//    
-    var textArea = $("<textarea>")
-    textArea.attr("id", "note" + i)
+    var textArea = $("<textarea>");
+    textArea.attr("id", "note" + i);
     if (timesOfDay[i - 1] === moment().format("hA")) {
-        textArea.attr("class", "description present")
+        textArea.attr("class", "description present");
     } else if (timesOfDay[i - 1] > moment().format("hA")) {
-        textArea.attr("class", "description future")
+        textArea.attr("class", "description future");
     } else {
-        textArea.attr("class", "description past")
+        textArea.attr("class", "description past");
     }
     //-----------Save buttons----------------------//
-    var myButton = $("<button>")
-    myButton.attr("id", "saveNote" + i)
-    myButton.attr("class", "saveBtn")
-    myButton.attr("data-id", i)
-    myButton.text("Save")
+    var myButton = $("<button>");
+    myButton.attr("id", "saveNote" + i);
+    myButton.attr("class", "saveBtn");
+    myButton.attr("data-id", i);
+    myButton.text("Save");
 
-    row.append(timeBlock, textArea, myButton)
-    container.append(row)
-    $(".appendTo").append(container)
+    row.append(timeBlock, textArea, myButton);
+    container.append(row);
+    $(".appendTo").append(container);
 }
 //--To save inserted text to local Storage after pressing save--//
 $(".appendTo").on("click", ".saveBtn", function () {
-    var notes = $("#note" + $(this).attr("data-id")).val()
+    var notes = $("#note" + $(this).attr("data-id")).val();
     console.log(notes);
     localStorage.setItem("todo" + $(this).attr("data-id"), JSON.stringify(notes));
 });
 //----------For Clear Schedule button----------//
 $("#clear").click(function () {
     localStorage.clear();
-    location.reload()
+    location.reload();
 });
+
+//---To display text in text area after refreshing browser---//
+localStorageReturnAndShowOnPage();
+
+function localStorageReturnAndShowOnPage() {
+    for (var j = 1; j < timesOfDay.length + 1; j++) {
+        var localStorageNote = JSON.parse(localStorage.getItem("todo" + j));
+
+        if (localStorageNote !== null) {
+            $("#note" + j).text(localStorageNote);
+        }
+    }
+}
